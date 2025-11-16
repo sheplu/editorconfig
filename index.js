c287c0bcaaa3ca22a9ba29a3ea06e10eb9f35ee7
Jean Burellier <jean.burellier@gmail.com>
2025-11-16 16:10:06 +0100

    feat: define file as constant

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 #!/usr/bin/env node
 
-import { copyFile } from 'node:fs/promises';
+import { writeFileSync } from "node:fs";
 
-export async function createEditorConfig(path = '.editorconfig') {
-	await copyFile('.editorconfig', path);
+const editorconfigContent = `root = true
+
+[*]
+indent_style = tab
+indent_size = 4
+tab_width = 4
+end_of_line = lf
+charset = utf-8
+spelling_language = en
+trim_trailing_whitespace = true
+insert_final_newline = true
+quote_type = single
+spaces_around_operators = true
+`;
+
+export function createEditorConfig(path = '.editorconfig') {
+	writeFileSync(path, editorconfigContent, 'utf8');
 };
 
-async function main() {
-	await createEditorConfig();
+function main() {
+	createEditorConfig();
 };
 
-await main();
+main();
